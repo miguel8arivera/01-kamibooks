f225212134d04a74e32a9c0b7ae72d3649b11848
Miguel Angel Ochoa <miguel.angel.8a.rivera@gmail.com>
2023-10-06 09:17:59 -0500

    feat(PT-01): componente Content.tsx fixed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from 'react';
 import data from '../books.json';
 
 export interface Author {
@@ -14,25 +15,67 @@ export interface Book {
   ISBN: string;
   author: Author;
 }
+const books: Book[] = data?.library?.map((data) => data.book);
+const genres: string[] = Array.from(new Set(books?.map((book) => book.genre)));
 
+console.log(books);
 export default function Content() {
-  const books: Book[] = data?.library?.map((data) => data.book);
-  console.log(books);
+  const [genre, setGenre] = useState<string>('');
+
+  /*   const matches = genre
+    ? books?.filter((book) => {
+        if (book.genre !== genre) return false;
+        return true;
+      })
+    : books; una forma mas simple de hacer, pero no es la mas optima*/
+
+  /*  const matches = useMemo(
+    () =>
+      genre
+        ? books?.filter((book) => {
+            if (book.genre !== genre) return false;
+            return true;
+          })
+        : books,
+    [genre]
+  ); Otra manera de mejor caliad pero no tan legible, tu decides cual usar */
+
+  const matches = useMemo(() => {
+    if (!genre) return books;
+
+    return books?.filter((book) => {
+      if (book.genre !== genre) return false;
+      return true;
+    });
+  }, [genre]);
   return (
-    <>
-      <article className="grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4">
-        {books?.map((book) => (
-          <div key={book.ISBN}>
+    <article className="grid gap-4">
+      <nav>
+        <select
+          value={genre}
+          onChange={(event) => setGenre(event.target.value)}
+        >
+          <option value="">Todos</option>
+          {genres?.map((genre) => (
+            <option key={genre} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
+      </nav>
+      <ul className="grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4">
+        {matches?.map((book) => (
+          <li key={book.ISBN}>
             <img
               className="aspect-[9/14] object-cover"
               src={book.cover}
               alt={book.title}
             />
             <p>{book.title}</p>
-          </div>
+          </li>
         ))}
-      </article>
-    </>
+      </ul>
+    </article>
   );
 }
 
